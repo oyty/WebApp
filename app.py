da3640bf29143a5daa4773dc6f0a7580f5de0696
oyty <cooloyty@gmail.com>
2016-01-26 21:28:36 +0800

    finish the console manager

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_locale():
 
 
 # Create directory for file fields to use
-file_path = os.path.join(os.path.dirname(__file__), 'files')
+file_path = os.path.join(os.path.dirname(__file__), 'static')
 try:
     os.mkdir(file_path)
 except OSError:
@@ -97,6 +97,22 @@ class User(db.Model, UserMixin):
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
+
+
+# 人才团队
+class PeopleTitle(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    subtitle = db.Column(db.String(), nullable=False)
+    title_in_english = db.Column(db.String(), nullable=False)
+    subtitle_in_english = db.Column(db.String(), nullable=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+class PeopleTitleAdmin(BaseModelView):
+    column_list = ['title', 'subtitle', 'title_in_english', 'subtitle_in_english']
 
 
 class People(db.Model):
@@ -168,51 +184,8 @@ class DescriptionAdmin(BaseModelView):
     column_list = ['people', 'desc']
 
 
-# Create model
-class Contact(db.Model):
-    # def __init__(self, mobile, qq, email, address):
-    #     self.mobile = mobile
-    #     self.qq = qq
-    #     self.email = email
-    #     self.address = address
 
-    id = db.Column(db.Integer, primary_key=True)
-    mobile = db.Column(db.String(20), nullable=False)
-    qq = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    address = db.Column(db.String(100), nullable=False)
-    qrcode = db.Column(db.Unicode(128), nullable=False)
-
-    # Required for admin interface. For python 3 please use __str__ instead.
-    def __unicode__(self):
-        return self.name
-
-
-# Add custom filter and standard FilterEqual to ModelView
-class ContactAdmin(BaseModelView):
-    # column_list = ['id', 'mobile', 'qq', 'email', 'address', 'path']
-    column_sortable_list = ['mobile', 'qq', 'email', 'address', 'qrcode']
-    column_list = ['mobile', 'QQ', 'email', 'address', 'qrcode']
-
-    def _list_thumbnail(view, context, model, name):
-        if not model.qrcode:
-            return ''
-        return Markup('<img src="%s">' % url_for('static',
-                                                 filename=form.thumbgen_filename(model.qrcode)))
-
-    column_formatters = {
-        'qrcode': _list_thumbnail
-    }
-
-    # Alternative way to contribute field is to override it completely.
-    # In this case, Flask-Admin won't attempt to merge various parameters for the field.
-    form_extra_fields = {
-        'qrcode': form.ImageUploadField('Image',
-                                        base_path=file_path,
-                                        thumbnail_size=(100, 100, True))
-    }
-
-
+# 公司简介
 class Company(db.Model):
     __tablename__ = 'company'
     id = db.Column(db.Integer(), primary_key=True)
@@ -274,14 +247,284 @@ class Profile(db.Model):
         return self.name
 
 
-class Service(db.Model):
+class ProfileTitle(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    sort_num = db.Column(db.Integer(), nullable=False)
     title = db.Column(db.String(), nullable=False)
-    link = db.Column(db.String(), nullable=False)
+    subtitle = db.Column(db.String(), nullable=False)
+    title_in_english = db.Column(db.String(), nullable=False)
+    subtitle_in_english = db.Column(db.String(), nullable=False)
 
     def __unicode__(self):
         return self.name
+
+
+class ProfileTitleAdmin(BaseModelView):
+    column_list = ['title', 'subtitle', 'title_in_english', 'subtitle_in_english']
+
+
+
+# 产品服务
+class ProductTitle(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    subtitle = db.Column(db.String(), nullable=False)
+    title_in_english = db.Column(db.String(), nullable=False)
+    subtitle_in_english = db.Column(db.String(), nullable=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ProductTitleAdmin(BaseModelView):
+    column_list = ['title', 'subtitle', 'title_in_english', 'subtitle_in_english']
+
+
+class ProductName(db.Model):
+    __tablename__ = 'productnames'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Unicode(64), nullable=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+@listens_for(ProductName, 'after_delete')
+def del_image(mapper, connection, target):
+    if target.path:
+        # Delete image
+        try:
+            os.remove(os.path.join(file_path, target.path))
+        except OSError:
+            pass
+
+        # Delete thumbnail
+        try:
+            os.remove(os.path.join(file_path,
+                                   form.thumbgen_filename(target.path)))
+        except OSError:
+            pass
+
+
+class ProductNameAdmin(BaseModelView):
+    column_display_pk = True
+    # form_columns = ['name', 'productdetails']
+    # column_list = ['id', 'name']
+
+
+class ProductDetail(db.Model):
+    __tablename__ = 'productdetails'
+    productdetail_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    productname_id = db.Column(db.Integer, db.ForeignKey('productnames.id'))
+    productname = db.relationship('ProductName', backref='productdetails')
+    detail = db.Column(db.Unicode(128), nullable=False)
+
+    def __unicode__(self):
+        return self.detail
+
+
+@listens_for(ProductDetail, 'after_delete')
+def del_image(mapper, connection, target):
+    if target.detail:
+        # Delete image
+        try:
+            os.remove(os.path.join(file_path, target.detail))
+        except OSError:
+            pass
+
+        # Delete thumbnail
+        try:
+            os.remove(os.path.join(file_path,
+                                   form.thumbgen_filename(target.detail)))
+        except OSError:
+            pass
+
+
+class ProductDetailAdmin(BaseModelView):
+    # column_display_pk = True
+    # form_columns = ['productname', 'productdetails']
+    # column_list = ['productname', 'productdetails']
+
+    def _list_thumbnail(view, context, model, name):
+        if not model.detail:
+            return ''
+        return Markup('<img src="%s">' % url_for('static',
+                                                 filename=form.thumbgen_filename(model.detail)))
+
+    column_formatters = {
+        'detail': _list_thumbnail
+    }
+
+    # Alternative way to contribute field is to override it completely.
+    # In this case, Flask-Admin won't attempt to merge various parameters for the field.
+    form_extra_fields = {
+        'detail': form.ImageUploadField('Image',
+                                       base_path=file_path,
+                                       thumbnail_size=(100, 100, True))
+    }
+
+
+# 项目案例
+class ProjectTitle(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    subtitle = db.Column(db.String(), nullable=False)
+    title_in_english = db.Column(db.String(), nullable=False)
+    subtitle_in_english = db.Column(db.String(), nullable=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ProjectTitleAdmin(BaseModelView):
+    column_list = ['title', 'subtitle', 'title_in_english', 'subtitle_in_english']
+
+
+class Project(db.Model):
+    __tablename__ = 'project'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    photo = db.Column(db.Unicode(128), nullable=False)
+
+    # Required for admin interface. For python 3 please use __str__ instead.
+    def __unicode__(self):
+        return self.name
+
+
+# Add custom filter and standard FilterEqual to ModelView
+class ProjectAdmin(BaseModelView):
+    # column_list = ['id', 'mobile', 'qq', 'email', 'address', 'path']
+    # column_sortable_list = ['mobile', 'qq', 'email', 'address', 'qrcode']
+    # column_list = ['mobile', 'QQ', 'email', 'address', 'qrcode']
+
+    def _list_thumbnail(view, context, model, name):
+        if not model.photo:
+            return ''
+        return Markup('<img src="%s">' % url_for('static',
+                                                 filename=form.thumbgen_filename(model.photo)))
+
+    column_formatters = {
+        'photo': _list_thumbnail
+    }
+
+    # Alternative way to contribute field is to override it completely.
+    # In this case, Flask-Admin won't attempt to merge various parameters for the field.
+    form_extra_fields = {
+        'photo': form.ImageUploadField('Image',
+                                        base_path=file_path,
+                                        thumbnail_size=(100, 100, True))
+    }
+
+
+class ProjectDetail(db.Model):
+    __tablename__ = 'projectdetails'
+    projectdetail_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    projects_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+    project = db.relationship('Project', backref='projectdetails')
+    detail = db.Column(db.Unicode(128), nullable=False)
+
+    def __unicode__(self):
+        return self.detail
+
+
+@listens_for(ProjectDetail, 'after_delete')
+def del_image(mapper, connection, target):
+    if target.detail:
+        # Delete image
+        try:
+            os.remove(os.path.join(file_path, target.detail))
+        except OSError:
+            pass
+
+        # Delete thumbnail
+        try:
+            os.remove(os.path.join(file_path,
+                                   form.thumbgen_filename(target.detail)))
+        except OSError:
+            pass
+
+
+class ProjectDetailAdmin(BaseModelView):
+    # column_display_pk = True
+    # form_columns = ['productname', 'productdetails']
+    # column_list = ['productname', 'productdetails']
+
+    def _list_thumbnail(view, context, model, name):
+        if not model.detail:
+            return ''
+        return Markup('<img src="%s">' % url_for('static',
+                                                 filename=form.thumbgen_filename(model.detail)))
+
+    column_formatters = {
+        'detail': _list_thumbnail
+    }
+
+    # Alternative way to contribute field is to override it completely.
+    # In this case, Flask-Admin won't attempt to merge various parameters for the field.
+    form_extra_fields = {
+        'detail': form.ImageUploadField('Image',
+                                       base_path=file_path,
+                                       thumbnail_size=(100, 100, True))
+    }
+
+
+# 联系方式
+class ContactTitle(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    subtitle = db.Column(db.String(), nullable=False)
+    title_in_english = db.Column(db.String(), nullable=False)
+    subtitle_in_english = db.Column(db.String(), nullable=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ContactTitleAdmin(BaseModelView):
+    column_list = ['title', 'subtitle', 'title_in_english', 'subtitle_in_english']
+
+
+class Contact(db.Model):
+    # def __init__(self, mobile, qq, email, address):
+    #     self.mobile = mobile
+    #     self.qq = qq
+    #     self.email = email
+    #     self.address = address
+
+    id = db.Column(db.Integer, primary_key=True)
+    mobile = db.Column(db.String(20), nullable=False)
+    qq = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    qrcode = db.Column(db.Unicode(128), nullable=False)
+
+    # Required for admin interface. For python 3 please use __str__ instead.
+    def __unicode__(self):
+        return self.name
+
+
+# Add custom filter and standard FilterEqual to ModelView
+class ContactAdmin(BaseModelView):
+    # column_list = ['id', 'mobile', 'qq', 'email', 'address', 'path']
+    column_sortable_list = ['mobile', 'qq', 'email', 'address', 'qrcode']
+    column_list = ['mobile', 'QQ', 'email', 'address', 'qrcode']
+
+    def _list_thumbnail(view, context, model, name):
+        if not model.qrcode:
+            return ''
+        return Markup('<img src="%s">' % url_for('static',
+                                                 filename=form.thumbgen_filename(model.qrcode)))
+
+    column_formatters = {
+        'qrcode': _list_thumbnail
+    }
+
+    # Alternative way to contribute field is to override it completely.
+    # In this case, Flask-Admin won't attempt to merge various parameters for the field.
+    form_extra_fields = {
+        'qrcode': form.ImageUploadField('Image',
+                                        base_path=file_path,
+                                        thumbnail_size=(100, 100, True))
+    }
 
 
 # Flask views
@@ -289,25 +532,34 @@ class Service(db.Model):
 def index():
     return render_template('index.html')
 
+
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
 
+
 @app.route('/product')
 def product():
-    return render_template('product.html')
+    title = ProductTitle.query.all()[0]
+    name = ProductName.query.all()
+    return render_template('product.html', producttitle=title, productname=name)
 
 @app.route('/talent')
 def talent():
     return render_template('talent.html')
 
+
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    title = ProjectTitle.query.all()[0]
+    projects = Project.query.all()
+    return render_template('projects.html', projecttitle=title, projects=projects)
+
 
 @app.route('/agency')
 def agency():
     return render_template('agency.html')
+
 
 @app.route('/contact')
 def contact():
@@ -322,15 +574,33 @@ admin = Admin(app,
 
 admin.add_view(BaseModelView(Role, db.session, name='角色', category='用户'))
 admin.add_view(BaseModelView(User, db.session, name='用户', category='用户'))
-# admin.add_view(PeopleAdmin(People, db.session, name='People', category='TalentTeam'))
-# admin.add_view(DescriptionAdmin(Description, db.session, name='Description', category='TalentTeam'))
-admin.add_view(CompanyAdmin(Company, db.session, name='名称&商标'))
-admin.add_view(BaseModelView(Slogan, db.session, name='口号'))
-admin.add_view(BaseModelView(Profile, db.session, name='简介'))
-admin.add_view(BaseModelView(Service, db.session, name='产品服务'))
+
+
+admin.add_view(CompanyAdmin(Company, db.session, name='名称&商标', category='简介'))
+admin.add_view(BaseModelView(Slogan, db.session, name='口号', category='简介'))
+
+
+admin.add_view(ProjectTitleAdmin(ProfileTitle, db.session, name='标题', category='公司简介'))
+admin.add_view(BaseModelView(Profile, db.session, name='简介', category='公司简介'))
+
+
+admin.add_view(ProductTitleAdmin(ProductTitle, db.session, name='标题', category='产品服务'))
+admin.add_view(ProductNameAdmin(ProductName, db.session, name='模块', category='产品服务'))
+admin.add_view(ProductDetailAdmin(ProductDetail, db.session, name='模块详情', category='产品服务'))
+
+
+admin.add_view(PeopleTitleAdmin(PeopleTitle, db.session, name='标题', category='人才团队'))
 admin.add_view(PeopleAdmin(People, db.session, name='人才', category='人才团队'))
 admin.add_view(DescriptionAdmin(Description, db.session, name='描述', category='人才团队'))
-admin.add_view(ContactAdmin(Contact, db.session, name='联系方式', menu_class_name='爱的'))
+
+
+admin.add_view(ProjectTitleAdmin(ProjectTitle, db.session, name='标题', category='项目案例'))
+admin.add_view(ProjectAdmin(Project, db.session, name='项目', category='项目案例'))
+admin.add_view(ProjectDetailAdmin(ProjectDetail, db.session, name='项目详情', category='项目案例'))
+
+
+admin.add_view(ContactTitleAdmin(ContactTitle, db.session, name='标题', category='联系方式'))
+admin.add_view(ContactAdmin(Contact, db.session, name='联系方式', category='联系方式'))
 
 
 @security.context_processor
